e5873b6c26803c391329aa413391d0d6bf6be8e9
drbrunomp <drbrunomp@gmail.com>
2024-07-25 11:02:37 -0300

    Códigos para calculadora

---
+++ Calculadora_básica/calc_basic.cpp
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<math.h>
+
+float value1,
+      value2,
+      result,
+      result2;
+char oper;
+
+main()
+{
+    printf("Exercicio calculadora em C++ \n");
+    printf("Entre com V1: \n");
+    scanf("%f",&value1);
+    printf("Entre com V2: \n");
+    scanf("%f",&value2);
+
+    printf("Entre com a operacao matematica:\n");
+    printf("+ --> SOMA; \n- --> SUBTRACAO; \n* --> MULTIPLICACAO; \n/ --> DIVISAO; \nP --> POTENCIACAO; \n");
+    scanf("%s",&oper);
+
+    switch(oper)
+    {
+        case '+':
+            result = value1 + value2;
+            printf("Resultado = %f",result);
+        break;
+        case '-':
+            result = value1 - value2;
+            printf("Resultado = %f",result);
+        break;
+        case '*':
+            result = value1 * value2;
+            printf("Resultado = %f",result);
+        break;
+        case '/':
+            result = value1 / value2;
+            printf("Resultado = %f",result);
+        break;
+        case 'P':
+            result = pow(value1, 2);
+            result2 = pow(value2, 2);
+            printf("Resultado 1 = %f",result);
+            printf("\nResultado 2 = %f",result2);
+        break;
+        default:
+            printf("\nOPERACAO INVALIDA");
+        break;
+    }
+
+    if(result < 10.0)
+    {
+        printf("\nRESULTADO MENOR QUE 10");
+    }
+    else if(result >= 10 && result < 100)
+    {
+        printf("\nRESULTADO ENTRE 10 E 100");
+    }
+    else
+    {
+        printf("\nRESULTADO ACIMA DE 100");
+    }
+}
